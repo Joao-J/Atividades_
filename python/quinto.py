cffa025aaa0f5015d259be98c02d71045b43e1b6
JOÃO VITOR <118869806+Joao-J@users.noreply.github.com>
2022-11-26 01:28:17 -0300

    Update quinto.py

--- python/quinto.py
+++ python/quinto.py
@@ -1,8 +1,8 @@
 def main():
-    x = int(input('Informe a unidade em metros a ser convertida em centimetro: '))
+    x = int(input('Informe a unidade em metros a ser convertida em centímetro: '))
     m = ' metro'
     if (x > 1):
         m += 's'
-    print( str(x) + m + ' é equivalente a ' + str( (x * 100) ) + ' centimetros')
+    print( str(x) + m + ' é equivalente a ' + str( (x * 100) ) + ' centímetros')
 
 main()
