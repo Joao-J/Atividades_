f7fde4398ae03ac1fdb1bcc98c67d894864e3d76
JOÃO VITOR <118869806+Joao-J@users.noreply.github.com>
2022-11-26 01:27:29 -0300

    Update quarto.py

--- python/quarto.py
+++ python/quarto.py
@@ -1,7 +1,7 @@
 def main():
     nota = []
     while (len(nota) < 4):
-        nota.append(float(input('Informe a nota do ' + str(len(nota) + 1) + 'º semestre : ' )))
+        nota.append(float(input('Informe a nota do ' + str(len(nota) + 1) + 'º bimestre : ' )))
     print('Sua média é ' + str(sum(nota)/len(nota)))
     
 main()
