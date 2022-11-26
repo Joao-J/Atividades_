6673c898da2e8940829bd5f4f780b9626a79c19a
JOÃO VITOR <118869806+Joao-J@users.noreply.github.com>
2022-11-26 06:18:58 -0300

    Add files via upload

---
+++ python/setimo.py
@@ -0,0 +1,7 @@
+def main():
+    ALTURA = int(input('Informe a altura do quadrado em centímetros: '))
+    BASE = ALTURA
+    AREA = BASE * ALTURA
+    print('A area do seu quadrado é de ' + str(AREA) + 'cm')
+
+main()
