e43e6ddda133443ed181ff50f51e2ec89d44a23e
JOÃO VITOR <118869806+Joao-J@users.noreply.github.com>
2022-11-25 23:18:09 -0300

    Create terceiro_v1.py

---
+++ python/terceiro_v1.py
@@ -0,0 +1,7 @@
+def main():
+    num1 = int(input('Informe um número '))
+    num2 = int(input('Informe outro número '))
+    soma = num1 + num2
+    print('A soma entre os números ' + str(num1) + '+' + str(num2) + ' = ' + str(soma))
+
+main()
